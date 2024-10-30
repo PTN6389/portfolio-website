c239912212461624b54429fa34d484bc2627d78c
PTN6389 <117026249+PTN6389@users.noreply.github.com>
2024-10-30 09:35:41 -0500

    changes for portfolio website

--- js/script.js
+++ js/script.js
@@ -1,3 +1,112 @@
+const projects = [
+    {
+        title: 'Pokemon App',
+        screenshot: '',
+        description: 'Small web application with HTML, CSS, and JavaScript that loads Pokemon data from an external API and enables the viewing of data points in detail.',
+        technologies: ['HTML', 'CSS', 'JavaScript'],
+        liveLink: 'https://ptn6389.github.io/pokemon-app/',
+        sourceLink: 'https://github.com/PTN6389/pokemon-app'
+},
+    {
+        title: 'Movie API',
+        screenshot: '',
+        description: 'Server-side component of a “movies” web application. The web application will provide users with access to information about different movies, directors, and genres. Users will be able to sign up, update their personal information, and create a list of their favorite movies.',
+        technologies: ['Node.js', 'Express', 'REST APIs', 'MongoDB', 'Mongoose', 'JSON', 'JavaScript', 'Postman', 'Heroku'],
+        liveLink: '',
+        sourceLink: 'https://github.com/PTN6389/movie_api'
+},
+    {
+        title: 'myFlix App',
+        screenshot: '',
+        description: 'React client-side code for an app called myFlix based on the existing Movie API server-side code (REST API and database).',
+        technologies: ['React', 'Parcel', 'Bootstrap', 'React Redux', 'Netlify'],
+        liveLink: 'https://myflix-ptn.netlify.app/',
+        sourceLink: 'https://github.com/PTN6389/myFlix-client'  
+    },
+    {
+        title: 'Meet App',
+        screenshot: '',
+        description: 'Serverless, progressive web application (PWA) with React using a test-driven development (TDD) technique. The application uses the Google Calendar API to fetch upcoming events.',
+        technologies: ['React', 'Test Driven Development(TDD) using Jest', 'AWS Lambda serverless function'],
+        liveLink: 'https://ptn6389.github.io/meet/',
+        sourceLink: 'https://github.com/PTN6389/meet'
+    },
+    {
+        title: 'Chat App',
+        screenshot: '',
+        description: 'Chat app for mobile devices using React Native that provides users with a chat interface and options to share images and their location.',
+        technologies: ['React Native', 'Expo', 'Google Firestore Database', 'Google Firebase Authentication', 'Firebase Cloud Storage', 'Gifted Chat Library'],
+        liveLink: '',
+        sourceLink: 'https://github.com/PTN6389/chat-app'
+    },
+    {
+        title: 'myFlix App',
+        screenshot: '',
+        description: 'Angular client-side for an application called myFlix based on the existing Movie API server-side code (REST API and database).',
+        technologies: ['Angular', 'Angular Material Design', 'TypeDoc', 'JSDoc'],
+        liveLink: 'https://ptn6389.github.io/myFlix-Angular-client/welcome',
+        sourceLink: 'https://github.com/PTN6389/myFlix-Angular-client'  
+    }
+]
+
+const technologies = [];
+
+projects.forEach(project => {
+    let projectElement = document.createElement('div');
+    projectElement.classList.add('grid__item');
+    projectElement.innerHTML = `    
+        <h3>${project.title}</h3>
+        <p>${project.description}</p>
+        <p><a href="${project.liveLink}" target="_blank">Live Demo</a></p>
+        <p><a href="${project.sourceLink}" target="_blank">Source Code</a></p>
+        <h4>Technologies:</h4>
+        <span>
+            ${project.technologies.map(tech => `${tech}`).join(',  ')}
+        </span>        
+    `
+    document.querySelector('.grid').appendChild(projectElement);
+});
+
+// Get distinct list of technologies from each project
+projects.forEach(project => {
+    project.technologies.forEach(tech => {
+        if(!technologies.includes(tech)) {
+            technologies.push(tech);
+        }
+    });
+    technologies.sort();
+});
+
+
+
+// Create function to display technologies in filter select
+(function createTechnologiesFilter() {
+    const filter = document.getElementById('filter');
+    technologies.forEach(tech => {
+        let option = document.createElement('option');
+        //option style should be dropdown with checkbox to the left
+
+        option.value = tech;
+        option.text = tech;
+        filter.appendChild(option);
+    });
+})();
+
+document.querySelector('.dropdown-btn').addEventListener('click', function() {
+    const dropdownList = document.querySelector('.dropdown-list');
+    dropdownList.style.display = dropdownList.style.display === 'block' ? 'none' : 'block';
+  });
+  
+  // Close the dropdown when clicking outside
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropdown-btn')) {
+      const dropdowns = document.querySelectorAll('.dropdown-list');
+      dropdowns.forEach(function(dropdown) {
+        dropdown.style.display = 'none';
+      });
+    }
+  };
+    
 (function() {
     let form = document.querySelector('#contact-form');
     let emailInput = document.querySelector('#email');
@@ -41,12 +150,17 @@
         return isValidEmail;
     }
 
-    form.addEventListener('submit',(e) => {
-        e.preventDefault(); //do not submit to server
-        if(validateForm()) {
-            alert('Success!');
-        }
-    })
+    if(form) {
+        form.addEventListener('submit',(e) => {
+            e.preventDefault(); //do not submit to server
+            if(validateForm()) {
+                alert('Success!');
+            }
+        })
 
-    emailInput.addEventListener('input',validateEmail);
+        emailInput.addEventListener('input',validateEmail);
+    }
+    
+
+    
 })();
